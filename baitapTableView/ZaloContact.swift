9551a01de0cf99d4d64669e5b3acdc4ec08d018b
Nguyen Chi Trung <chitrung.nguyen.vn@gmail.com>
2022-11-09 15:46:28 +0700

    Đẩy pull request

---
+++ baitapTableView/ZaloContact.swift
@@ -0,0 +1,23 @@
+//
+//  ZaloContact.swift
+//  baitapTableView
+//
+//  Created by trung on 09/11/2022.
+//
+
+import UIKit
+
+class ZaloContact: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
